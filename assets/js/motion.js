c74c8f453929e920104c072adb613dea6cea7eae
choustof <petit.christophe.91@gmail.com>
2018-03-13 02:27:45 +0100

    Motion detection active
    Detection des mouvements en direct

--- assets/js/motion.js
+++ assets/js/motion.js
@@ -1,103 +1,137 @@
-var sensitivity, temp1Canvas, temp1Context, temp2Canvas, temp2Context, topLeft, bottomRight;
-function initialize() {
-            sensitivity = 40;
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+window.URL = window.URL || window.webkitURL;
 
-            if(!temp1Canvas) {
-                temp1Canvas = document.createElement('canvas');
-                temp1Context = temp1Canvas.getContext("2d");
-            }
+function gotStream(stream) 
+{
+    if (window.URL) 
+    {   camvideo.src = window.URL.createObjectURL(stream);   } 
+    else // Opera
+    {   camvideo.src = stream;   }
+    camvideo.onerror = function(e) 
+    {   stream.stop();   };
+    stream.onended = noStream;
+}
+function noStream(e) 
+{
+    var msg = 'No camera available.';
+    if (e.code == 1) 
+    {   msg = 'User denied access to use camera.';   }
+    document.getElementById('errorMessage').textContent = msg;
+}
 
-            if(!temp2Canvas) {
-                temp2Canvas = document.createElement('canvas');
-                temp2Context = temp2Canvas.getContext("2d");
-            }
+// assign global variables to HTML elements
+var videoCanvas = document.getElementById( 'videoCanvas' );
+var videoContext = videoCanvas.getContext( '2d' );
+var layer2Canvas = document.getElementById( 'layer2' );
+var layer2Context = layer2Canvas.getContext( '2d' );
+var blendCanvas  = document.getElementById( "blendCanvas" );
+var blendContext = blendCanvas.getContext('2d');
 
-            topLeft = [Infinity,Infinity];
-            bottomRight = [0,0];
-        }
-
-        function compare(image1, image2, width, height) {
-            initialize();
-
-            if(!image1 || !image2) {
-                return;
-            }
-
-            temp1Context.clearRect(0,0,100000,100000);
-            temp1Context.clearRect(0,0,100000,100000);
-
-            temp1Context.drawImage(image1, 0, 0, width, height);
-            temp2Context.drawImage(image2, 0, 0, width, height);
-
-
-            for(var y = 0; y < height; y++) {
-                for(var x = 0; x <  width; x++) {
-                    var pixel1 = temp1Context.getImageData(x,y,1,1);
-                    var pixel1Data = pixel1.data;
-
-                    var pixel2 = temp2Context.getImageData(x,y,1,1);
-                    var pixel2Data = pixel2.data;
-
-                    if(comparePixel(pixel1Data, pixel2Data) == false) {
-                        setTopLeft(x,y);
-                        setBottomRight(x,y);
-                    }                   
-                }
-            }
-
-            return {
-                'topLeft': topLeft,
-                'bottomRight': bottomRight
-            }
-        }
-
-        function comparePixel(p1, p2) {
-            var matches = true;
-
-            for(var i = 0; i < p1.length; i++) {
-                var t1 = Math.round(p1[i]/10)*10;
-                var t2 = Math.round(p2[i]/10)*10;
-
-                if(t1 != t2) {
-                    if((t1+sensitivity < t2 || t1-sensitivity > t2)) {
-                        matches = false;
-                    }
-                }
-            }
-
-            return matches;
-        }
-
-        function setTopLeft(x,y) {
-            if(x < topLeft[0] ) {
-                topLeft[0] = x;
-            }
-            if(y < topLeft[1]) {
-                topLeft[1] = [y];
-            }
-        }
-
-        function setBottomRight(x,y) {
-            if(x > bottomRight[0]) {
-                bottomRight[0] = [x];
-            }
-            if(y > bottomRight[1]) {
-                bottomRight[1] = [y];
-            }
-        }
-
+// these changes are permanent
+videoContext.translate(320, 0);
+videoContext.scale(-1, 1);
+        
+// background color if no video present
+videoContext.fillStyle = '#005337';
+videoContext.fillRect( 0, 0, videoCanvas.width, videoCanvas.height );               
+var buttons = [];
+var button1 = new Image();
+button1.src ="images/SquareRed.png";
+var buttonData1 = { name:"red", image:button1, x:320 - 96 - 30, y:10, w:32, h:32 };
+buttons.push( buttonData1 );
+var button2 = new Image();
+button2.src ="images/SquareGreen.png";
+var buttonData2 = { name:"green", image:button2, x:320 - 64 - 20, y:10, w:32, h:32 };
+buttons.push( buttonData2 );
+var button3 = new Image();
+button3.src ="images/SquareBlue.png";
+var buttonData3 = { name:"blue", image:button3, x:320 - 32 - 10, y:10, w:32, h:32 };
+buttons.push( buttonData3 );
+// start the loop               
+animate();
+function animate() 
+{
+    requestAnimationFrame( animate );
     
-function captureImage(append) {
-            var canvas = document.createElement('canvas');
-            canvas.width = screen.width;
-            canvas.height = screen.height;
-            canvas.getContext('2d').drawImage(webCamWindow, 0, 0, width, height);
-
-            var pngImage = canvas.toDataURL("image/png"); 
+    render();   
+    blend();    
+    checkAreas();
+}
+function render() 
+{   
+    if ( video.readyState === video.HAVE_ENOUGH_DATA ) 
+    {
+        // mirror video
+        videoContext.drawImage( video, 0, 0, videoCanvas.width, videoCanvas.height );
+        for ( var i = 0; i < buttons.length; i++ )
+            layer2Context.drawImage( buttons[i].image, buttons[i].x, buttons[i].y, buttons[i].w, buttons[i].h );        
+    }
+}
+var lastImageData;
+function blend() 
+{
+    var width  = videoCanvas.width;
+    var height = videoCanvas.height;
+    // get current webcam image data
+    var sourceData = videoContext.getImageData(0, 0, width, height);
+    // create an image if the previous image doesn’t exist
+    if (!lastImageData) lastImageData = videoContext.getImageData(0, 0, width, height);
+    // create a ImageData instance to receive the blended result
+    var blendedData = videoContext.createImageData(width, height);
+    // blend the 2 images
+    differenceAccuracy(blendedData.data, sourceData.data, lastImageData.data);
+    // draw the result in a canvas
+    blendContext.putImageData(blendedData, 0, 0);
+    // store the current webcam image
+    lastImageData = sourceData;
+}
+function differenceAccuracy(target, data1, data2) 
+{
+    if (data1.length != data2.length) return null;
+    var i = 0;
+    while (i < (data1.length * 0.25)) 
+    {
+        var average1 = (data1[4*i] + data1[4*i+1] + data1[4*i+2]) / 3;
+        var average2 = (data2[4*i] + data2[4*i+1] + data2[4*i+2]) / 3;
+        var diff = threshold(fastAbs(average1 - average2));
+        target[4*i]   = diff;
+        target[4*i+1] = diff;
+        target[4*i+2] = diff;
+        target[4*i+3] = 0xFF;
+        ++i;
+    }
+}
+function fastAbs(value) 
+{
+    return (value ^ (value >> 31)) - (value >> 31);
+}
+function threshold(value) 
+{
+    return (value > 0x15) ? 0xFF : 0;
+}
+// check if white region from blend overlaps area of interest (e.g. triggers)
+function checkAreas() 
+{
+    for (var b = 0; b < buttons.length; b++)
+    {
+        // get the pixels in a note area from the blended image
+        var blendedData = blendContext.getImageData( buttons[b].x, buttons[b].y, buttons[b].w, buttons[b].h );
             
-            if(append) {
-                append.appendChild(canvas); 
-            }
-
-            return canvas;
+        // calculate the average lightness of the blended data
+        var i = 0;
+        var sum = 0;
+        var countPixels = blendedData.data.length * 0.25;
+        while (i < countPixels) 
+        {
+            sum += (blendedData.data[i*4] + blendedData.data[i*4+1] + blendedData.data[i*4+2]);
+            ++i;
+        }
+        // calculate an average between of the color values of the note area [0-255]
+        var average = Math.round(sum / (3 * countPixels));
+        if (average > 50) // more than 20% movement detected
+        {
+            console.log( "Mouvement au niveau de la zone " + buttons[b].name + " ." ); // do stuff
+        
+        }
+    }
 }
